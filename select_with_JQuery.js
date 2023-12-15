378950fc428c90340ae8867ea21f8e02fb2d25ea
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-15 16:24:10 +0530

    href modification

--- select_with_JQuery.js
+++ select_with_JQuery.js
@@ -5,3 +5,6 @@ $("button").text("Don't click here")
 
 //for the innerHTML
 $("button").html("<em>Don't click here</em>")
+
+//setting the attribute prop
+$("a").attr("href", "https://www.pinterest.ca/pin/367536019565892466/")
