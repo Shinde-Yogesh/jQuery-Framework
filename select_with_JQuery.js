8790646ff42b9d8e171db5e0d005e656f249426c
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-15 15:49:06 +0530

    set the properties using jquery

--- select_with_JQuery.js
+++ select_with_JQuery.js
@@ -1,1 +1,11 @@
-$("h1").css("color","Black")
+// $("h1").css("color","Black")
+//select the
+// const allh1 = document.querySelector("h1")
+// console.log(allh1)
+
+//using jquery
+$("h1").css("color", "pink");
+console.log($("h1").css("color"))
+
+//for check the font sixe
+console.log($("h1").css("font-size"))
