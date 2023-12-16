c77346add4582490231dbcfc25074da499bd3637
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-16 15:37:58 +0530

    add  the button and remove the button

--- select_with_JQuery.js
+++ select_with_JQuery.js
@@ -1,3 +1,14 @@
 $("h1").on("mouseover", function () {
-  $("h1").css("color", "yellow");
+  $("h1").css("color", "lite blue");
 });
+
+
+//new button created
+$("h1").before("<button> prev </button>");
+$("h1").after("<button> next </button>");
+
+//before and the after addt the before the attribute
+//and the append and prepend add into the button tag
+
+//remove the button
+$("button").remove();
