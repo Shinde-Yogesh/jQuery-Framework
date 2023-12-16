01290faa943e5ff2ca7b698e0038ee7420f5fb03
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-16 16:00:21 +0530

    multiple method on one attribute

--- select_with_JQuery.js
+++ select_with_JQuery.js
@@ -1,14 +1,10 @@
-$("h1").on("mouseover", function () {
-  $("h1").css("color", "lite blue");
+//for custom animation
+// $("button").on("click", function () {
+//   $("h1").animate({ opacity: 0.5 }); //second parameter must be numeric value
+// });
+
+
+//for multiple
+$("button").on("click", function() {
+  $("h1").slideUp().slideDown().animate({ opacity: 0.5 }); //second parameter must be numeric value
 });
-
-
-//new button created
-$("h1").before("<button> prev </button>");
-$("h1").after("<button> next </button>");
-
-//before and the after addt the before the attribute
-//and the append and prepend add into the button tag
-
-//remove the button
-$("button").remove();
