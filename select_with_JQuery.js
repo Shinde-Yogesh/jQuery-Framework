b9f9ea3f752fcbcfab710267ef82e18481a0ee67
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-16 13:37:37 +0530

    eventLisner for the single attribute

--- select_with_JQuery.js
+++ select_with_JQuery.js
@@ -1,10 +1,4 @@
-$("h1").text("Good Day")
-
-//on the class level
-$("button").text("Don't click here")
-
-//for the innerHTML
-$("button").html("<em>Don't click here</em>")
-
-//setting the attribute prop
-$("a").attr("href", "https://www.pinterest.ca/pin/367536019565892466/")
+$("h1").click(function ()
+{
+    $("h1").css("color","yellow")
+})
