5911f3417193fd3ab4c26a52b4f30fbd868defa1
Shinde Yogesh <yogeshshinde2324@gmail.com>
2023-12-16 14:08:10 +0530

    multiple event using the on keyword

--- select_with_JQuery.js
+++ select_with_JQuery.js
@@ -1,28 +1,3 @@
-// for (var i = 0; i < 5; i++) {
-//   document.querySelectorAll("button")[i].addEventListener("click", function () {
-//     document.querySelector("h1").style.color = "yellow";
-//   });
-// }
-
-//using the jquery
-$("button").click(function()
-{
-    $("h1").css("color","cyan")
-})
-
-$("input").keypress(function(event)
-{
-    console.log(event.key)
-})
-
-//for the body
-$("body").keypress(function(event)
-{
-    console.log(event.key)
-})
-
-//change the h1 text
-$("body").keypress(function(event)
-{
-    $("h1").text(event.key)
-})
+$("h1").on("mouseover", function () {
+  $("h1").css("color", "yellow");
+});
