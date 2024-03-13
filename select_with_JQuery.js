544bc53decae3c6ed3d68a5249e19dcf519a8c1f
Yogesh Shinde <yogeshshinde2324@gmail.com>
2024-03-13 23:55:08 +0530

    Update select_with_JQuery.js

--- select_with_JQuery.js
+++ select_with_JQuery.js
@@ -2,8 +2,6 @@
 // $("button").on("click", function () {
 //   $("h1").animate({ opacity: 0.5 }); //second parameter must be numeric value
 // });
-
-
 //for multiple
 $("button").on("click", function() {
   $("h1").slideUp().slideDown().animate({ opacity: 0.5 }); //second parameter must be numeric value
